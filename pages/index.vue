704c8e4705c56bddc96c4f261402ff28558bdf0a
Christian <Zettrex@hotmail.com>
2020-02-08 18:07:52 +0100

    updated page

--- pages/index.vue
+++ pages/index.vue
@@ -8,18 +8,7 @@
       <h2 class="subtitle">
         MA2-9
       </h2>
-      <div class="links">
-        <a href="https://nuxtjs.org/" target="_blank" class="button--green">
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <p>It works!!!</p>
     </div>
   </div>
 </template>
